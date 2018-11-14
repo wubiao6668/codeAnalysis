98b626f9f7f73cb5143736fa38fcb1e146c6821d
wubiao <wubiao@missfresh.cn>
2018-11-14 15:29:35 +0800

    init

---
+++ src/main/java/com/code/analysis/CodeAnalysisApplication.java
@@ -0,0 +1,12 @@
+package com.code.analysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeAnalysisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeAnalysisApplication.class, args);
+    }
+}
